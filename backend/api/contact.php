6f44a9c6824a94cb179d9f8deb593adbf0e9e68e
Varshini Kamala <varshinikamala14@gmail.com>
2026-07-04 17:18:22 +0530

    CORS

--- backend/api/contact.php
+++ backend/api/contact.php
@@ -1,9 +1,5 @@
 <?php
-
-header("Access-Control-Allow-Origin: https://kamala-portfolio-five.vercel.app");
-// Or use * temporarily for testing:
-// header("Access-Control-Allow-Origin: *");
-
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
